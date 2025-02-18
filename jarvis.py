5649e40a75e34c06627f93b8986c3a30b1e7310c
Lainpilled Hat <lainedhat@gmail.com>
2025-02-17 23:29:15 -0300

    incomplete implementation, quite buggy

--- jarvis.py
+++ jarvis.py
@@ -8,21 +8,30 @@ engine = pyttsx3.init()
 
 time.sleep(1)
 
-engine.say("Typing A")
-engine.runAndWait()
-keyboard.press('A')
-keyboard.release('A')
-def speech_Rec():
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
-        print("Say Something!")
-        audio = r.listen(source)
+# engine.say("Typing A")
+# engine.runAndWait()
+# keyboard.press('A')
+# keyboard.release('A')
+def contains_word(word):
+    return text.contains(word)
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print("Say Something!")
+    audio = r.listen(source)
+    try:
+            text = r.recognize_google(audio)
+            print("You said " + text)
+            if text.contains("Jarvis"):
+                lines = text.split()
+                if any(contains_word(keyword)) for keyword in ["send", "chat", "in chat", "text", "match", "maths", "math", "teen", ]:
+                    target_team = None
+                    if contains_word("team") or contains_word("teen"):
 
-        try:
-            print("You said " + r.recognize_google(audio))
-        except sr.UnknownValueError:
-            print("I couldn't understand you, ma'am")
-        except sr.RequestError as e:
-            print("could not request results from google; {0}".format(e))
 
+    except sr.UnknownValueError:
+        print("I couldn't understand you, ma'am")
+    except sr.RequestError as e:
+        print("could not request results from google; {0}".format(e))
 
+def send_message(message, team):
+ pass
